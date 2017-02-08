eec2acfe96b3ca2585c6b6edd9302412719f14bb
Benaiah Mischenko <benaiah@mischenko.com>
2017-02-08 00:02:50 -0800

    Move boolStack to new file

--- json/json.go
+++ json/json.go
@@ -56,56 +56,6 @@ func writeline(dest io.Writer, line *bytes.Buffer, indentLevel int) {
 	line.Truncate(0)
 }
 
-// This type keeps track of whether we're in an array or not. It works
-// as follows:
-//
-// - When we hit an opening object delimiter, we push `false`
-// - When we hit an opening array delimiter, we push `true`
-// - When we hit a closing delimiter, we pop
-//
-// This is needed in order to tell if an incoming string is a key or a
-// value. Without a stack to keep track of it, arrays with nested
-// structures would be impossible to keep track of.
-type boolStack struct {
-	s []bool
-}
-
-func newBoolStack() *boolStack {
-	return &boolStack{s: make([]bool, 0)}
-}
-
-func (s *boolStack) push(b bool) {
-	s.s = append(s.s, b)
-}
-
-func (s *boolStack) pop() bool {
-	l := len(s.s)
-	// We don't care about over-popping, as the json decoder will already
-	// error out if there are mismatched tokens. When the array is empty,
-	// we just return `false`
-	if l == 0 {
-		return false
-	}
-
-	res := s.s[l-1]
-	s.s = s.s[:l-1]
-	return res
-}
-
-func (s *boolStack) peek() bool {
-	l := len(s.s)
-	if l == 0 {
-		return false
-	}
-
-	res := s.s[l-1]
-	return res
-}
-
-func (s *boolStack) len() int {
-	return len(s.s)
-}
-
 func Fprint(dest io.Writer, jsonInput []byte, settings Settings) error {
 	writer := newColorWriter(settings.Colors, settings.Highlight)
 
